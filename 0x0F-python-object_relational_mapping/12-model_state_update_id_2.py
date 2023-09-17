d5c5a17788ff7580449702c170940badd8b14d02
Justonchari <justinonchari@gmail.com>
2023-09-17 13:16:39 -0700

    update state

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""a script that lists all State objects from the database hbtn_0e_6_usa
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import Session
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(bind=engine)
+    session = Session(bind=engine)
+    instance = session.query(State).filter(State.id == 2).first()
+    instance.name = 'New Mexico'
+    session.commit()
